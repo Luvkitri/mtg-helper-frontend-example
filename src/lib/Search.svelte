20557213d90e6889b34fb0bfb2a3f45ccf1616e8
Jan Jędrzejewski <janjedrzejewski@gmail.com>
2024-05-28 19:19:03 +0200

    Adjusted Search component

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -3,28 +3,35 @@
 	let autocompleteSuggestions = [];
 	let query = '';
 
-	async function fetchAutocompleteSuggestions() {
-		if (!query) {
+	async function fetchAutocompleteSuggestions(event) {
+		console.log(arguments)
+		if (!query || query.length < 2) {
 			autocompleteSuggestions = [];
 			return;
 		}
+
+		if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
+			event.preventDefault();
+		}
+
 		try {
 			const response = await fetch(`http://localhost:8000/autocomplete/cards?q=${query}`);
-			/** @type {import('$lib/types').AutocompleteSuggestion[]} */
+			/** @type string[] */
 			const data = await response.json();
 			console.log(data);
-			autocompleteSuggestions = data.map((suggestion) => suggestion.name) || [];
+			autocompleteSuggestions = data || [];
 		} catch (error) {
 			console.error(error);
 		}
 	}
 
-	function debounce(callback, delay) {
-		let timeout;
+	function debounce(callback, delay, event) {
+		/** @type number | undefined */
+		let timeout = undefined;
 
 		return (...args) => {
 			clearTimeout(timeout);
-			timeout = setTimeout(() => callback(...args), delay);
+			timeout = setTimeout(callback, delay, ...args);
 		};
 	}
 </script>
@@ -37,7 +44,8 @@
 		placeholder="Search..."
 		class="z-10 rounded-lg border border-gray-300 p-4 hover:border-blue-500 focus:border-blue-500 focus:outline-none"
 		bind:value={query}
-		on:keyup={debounce(() => fetchAutocompleteSuggestions(), 300)}
+		on:keyup={debounce(fetchAutocompleteSuggestions, 300)}
+		
 	/>
 	{#if autocompleteSuggestions.length > 0}
 		<div class="z-0 -mt-4 border-x border-b border-gray-300 pt-4 flex flex-col">
