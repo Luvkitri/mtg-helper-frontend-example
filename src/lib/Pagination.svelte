ea4c5d3beba3d6651cd950b2bbd400ed5e55f822
Jan Jędrzejewski <janjedrzejewski@gmail.com>
2024-06-19 17:12:42 +0200

    Pagination POC

---
+++ src/lib/Pagination.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div class="flex items-center gap-2">
+  <button class="border rounded w-12 h-12" type="button">&lt;</button>
+  <button class="border rounded w-12 h-12" type="button">&lt;&lt;</button>
+  <div class="flex items-center justify-center w-12 h-12">23</div>
+  <button class="border rounded w-12 h-12" type="button">&gt;</button>
+  <button class="border rounded w-12 h-12" type="button">&gt;&gt;</button>
+</div>
