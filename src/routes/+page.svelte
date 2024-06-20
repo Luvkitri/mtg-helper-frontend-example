fe561256ecbb970a21faacfe979390cb80962b57
Jan Jędrzejewski <janjedrzejewski@gmail.com>
2024-06-20 18:25:32 +0200

    Updated pagination state logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,64 @@
 	import Search from '$lib/Search.svelte';
 	import Pagination from '$lib/Pagination.svelte';
 
+	let selectedCardId;
 	let similarCards = [];
+	let total = 0;
+	let page = 1;
+	let pageCount = 0;
+	let limit = 10;
+	let offset = 0;
 
-	async function fetchSimilarCards (card_id) {
+	function setSelectedCardId(card_id) {
+		selectedCardId = card_id;
+ 	}
+
+ 	$: if (selectedCardId) {
+ 		fetchSimilarCards();
+ 	}
+	
+	async function fetchSimilarCards() {
 		try {
-			const response = await fetch(`http://localhost:8000/cards/similar/${card_id}`);
+			const response = await fetch(`http://localhost:8000/cards/similar/${selectedCardId}?limit=${limit}&offset=${offset}`);
 			const responseData = await response.json();
 			similarCards = responseData.data;
-			console.log(responseData.metadata);
+			limit = responseData.limit;
+			offset = responseData.offset;
+			total = responseData.total
+
+			pageCount = Math.ceil(total / limit); 
 		} catch (error) {
 			console.error(error);
 		}
 	}
 
+	async function setPage(nextPage) {
+		try {
+			if (nextPage > pageCount || nextPage < 0) {
+				return;
+			}
+			
+			page = nextPage;
+			offset = (page - 1) * limit;
+			await fetchSimilarCards();
+		} catch (error) {			
+			console.error(error);
+		}
+	} 
 </script>
 
 
 <div class="flex flex-col items-center w-full">
 	<h1 class="text-3xl font-bold">MTG Helper</h1>
 	<div class="w-96 flex p-4">
-		<Search fetchSimilarCards ={fetchSimilarCards } />
+		<Search setSelectedCardId={setSelectedCardId} />
 	</div>
 	<div class="grid grid-cols-5 gap-4">
 		{#each similarCards as card}
 			<div><img width="250" src={`https://cards.scryfall.io/normal/front/${card.scryfallId[0]}/${card.scryfallId[1]}/${card.scryfallId}.jpg`} alt={`card-${card.uuid}`}/></div>
 		{/each}
 	</div>
-	<Pagination />
+	<Pagination page={page} pageCount={pageCount} setPage={setPage} />
 </div>
 
 <p>
